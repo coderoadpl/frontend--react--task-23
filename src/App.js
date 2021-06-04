709eb2446fec62db9837074513ef24858d8851d2
Mateusz Choma <kontakt@coderoad.pl>
2021-06-04 09:18:47 +0200

    Condtional list rendering basd on state

--- src/App.js
+++ src/App.js
@@ -3,23 +3,45 @@ import React from 'react'
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 export class App extends React.Component {
+  state = {
+    isShowed: true
+  }
+
+  toggle = () => {
+    this.setState((prevState) => ({
+      isShowed: !prevState.isShowed
+    }))
+  }
+
   render () {
+    const { isShowed } = this.state
+
     return (
       <div>
-        <button>
-          HIDE
+        <button
+          style={{
+            cursor: 'pointer'
+          }}
+          onClick={this.toggle}
+        >
+          {isShowed ? 'HIDE' : 'SHOW'}
         </button>
-        <ul>
-          {
-          numbers.map((number) => {
-            return (
-              <li key={number}>
-                {number}
-              </li>
-            )
-          })
-        }
-        </ul>
+        {
+          isShowed ?
+            <ul>
+              {
+                numbers.map((number) => {
+                  return (
+                    <li key={number}>
+                      {number}
+                    </li>
+                  )
+                })
+              }
+            </ul>
+            :
+            null
+      }
       </div>
     )
   }
