dc0d355ca95aefa1608b6c4b35187f7e052baac2
Mateusz Choma <kontakt@coderoad.pl>
2021-06-04 09:13:20 +0200

    Function component replaced by class

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-export const App = () => {
-  return (
-    <div>
-      <button>
-        HIDE
-      </button>
-      <ul>
-        {
+export class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <button>
+          HIDE
+        </button>
+        <ul>
+          {
           numbers.map((number) => {
             return (
               <li key={number}>
@@ -18,9 +19,10 @@ export const App = () => {
             )
           })
         }
-      </ul>
-    </div>
-  )
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App
